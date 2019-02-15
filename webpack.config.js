460b48f3fceceaff2446aa9c566c8cb61f7463e0
jamesscreen <jamesscreen@yandex.ru>
2019-02-15 16:45:24 +0300

    Настроена работа с *.scss файлами

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 }
